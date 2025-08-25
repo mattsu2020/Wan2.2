8e5484b016e9aa00d6e77599b47f6beed1f3c1f6
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-08-25 17:55:26 +0900

    Use float16 precision

--- wan/configs/shared_config.py
+++ wan/configs/shared_config.py
@@ -7,11 +7,9 @@ wan_shared_cfg = EasyDict()
 
 # dtype
 # Users can modify this option in configuration files to switch
-# the precision of all model parameters. Half precision (float16)
-# inference is supported when FSDP is disabled.
-wan_shared_cfg.dtype = (torch.float16
-                        if torch.backends.mps.is_available()
-                        else torch.bfloat16)
+# the precision of all model parameters. Force half precision
+# (float16) for reduced memory consumption.
+wan_shared_cfg.dtype = torch.float16
 
 # t5
 wan_shared_cfg.t5_model = 'umt5_xxl'
@@ -19,7 +17,7 @@ wan_shared_cfg.t5_dtype = wan_shared_cfg.dtype
 wan_shared_cfg.text_len = 512
 
 # transformer
-wan_shared_cfg.param_dtype = wan_shared_cfg.dtype
+wan_shared_cfg.param_dtype = torch.float16
 
 # inference
 wan_shared_cfg.num_train_timesteps = 1000
