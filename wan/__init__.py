f39978f26dd5249bd622d1453e8760ff20be9fe7
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-08-25 18:52:56 +0900

    feat: set MPS fallback in package init

--- wan/__init__.py
+++ wan/__init__.py
@@ -1,4 +1,12 @@
 # Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
+
+import os
+
+import torch
+
+if torch.backends.mps.is_available():
+    os.environ.setdefault("PYTORCH_ENABLE_MPS_FALLBACK", "1")
+
 from . import configs, distributed, modules
 from .image2video import WanI2V
 from .text2video import WanT2V
