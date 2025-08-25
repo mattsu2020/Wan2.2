3664a82c5e94c953854dfb7f9615a055b1941dd1
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-08-25 17:55:29 +0900

    feat: add optional attention backends

--- wan/modules/attention.py
+++ wan/modules/attention.py
@@ -3,12 +3,14 @@ import torch
 
 try:
     import flash_attn_interface
+
     FLASH_ATTN_3_AVAILABLE = True
 except ModuleNotFoundError:
     FLASH_ATTN_3_AVAILABLE = False
 
 try:
     import flash_attn
+
     FLASH_ATTN_2_AVAILABLE = True
 except ModuleNotFoundError:
     FLASH_ATTN_2_AVAILABLE = False
@@ -18,6 +20,7 @@ try:
         LowerTriangularMask,
         memory_efficient_attention,
     )
+
     XFORMERS_AVAILABLE = True
 except ModuleNotFoundError:
     XFORMERS_AVAILABLE = False
@@ -25,8 +28,8 @@ except ModuleNotFoundError:
 import warnings
 
 __all__ = [
-    'flash_attention',
-    'attention',
+    "flash_attention",
+    "attention",
 ]
 
 
@@ -36,7 +39,7 @@ def flash_attention(
     v,
     q_lens=None,
     k_lens=None,
-    dropout_p=0.,
+    dropout_p=0.0,
     softmax_scale=None,
     q_scale=None,
     causal=False,
@@ -60,7 +63,7 @@ def flash_attention(
     """
     half_dtypes = (torch.float16, torch.bfloat16)
     assert dtype in half_dtypes
-    assert q.device.type == 'cuda' and q.size(-1) <= 256
+    assert q.device.type == "cuda" and q.size(-1) <= 256
 
     # params
     b, lq, lk, out_dtype = q.size(0), q.size(1), k.size(1), q.dtype
@@ -71,9 +74,9 @@ def flash_attention(
     # preprocess query
     if q_lens is None:
         q = half(q.flatten(0, 1))
-        q_lens = torch.tensor(
-            [lq] * b, dtype=torch.int32).to(
-                device=q.device, non_blocking=True)
+        q_lens = torch.tensor([lq] * b, dtype=torch.int32).to(
+            device=q.device, non_blocking=True
+        )
     else:
         q = half(torch.cat([u[:v] for u, v in zip(q, q_lens)]))
 
@@ -81,9 +84,9 @@ def flash_attention(
     if k_lens is None:
         k = half(k.flatten(0, 1))
         v = half(v.flatten(0, 1))
-        k_lens = torch.tensor(
-            [lk] * b, dtype=torch.int32).to(
-                device=k.device, non_blocking=True)
+        k_lens = torch.tensor([lk] * b, dtype=torch.int32).to(
+            device=k.device, non_blocking=True
+        )
     else:
         k = half(torch.cat([u[:v] for u, v in zip(k, k_lens)]))
         v = half(torch.cat([u[:v] for u, v in zip(v, k_lens)]))
@@ -96,7 +99,7 @@ def flash_attention(
 
     if version is not None and version == 3 and not FLASH_ATTN_3_AVAILABLE:
         warnings.warn(
-            'Flash attention 3 is not available, use flash attention 2 instead.'
+            "Flash attention 3 is not available, use flash attention 2 instead."
         )
 
     # apply attention
@@ -106,34 +109,40 @@ def flash_attention(
             q=q,
             k=k,
             v=v,
-            cu_seqlens_q=torch.cat([q_lens.new_zeros([1]), q_lens]).cumsum(
-                0, dtype=torch.int32).to(q.device, non_blocking=True),
-            cu_seqlens_k=torch.cat([k_lens.new_zeros([1]), k_lens]).cumsum(
-                0, dtype=torch.int32).to(q.device, non_blocking=True),
+            cu_seqlens_q=torch.cat([q_lens.new_zeros([1]), q_lens])
+            .cumsum(0, dtype=torch.int32)
+            .to(q.device, non_blocking=True),
+            cu_seqlens_k=torch.cat([k_lens.new_zeros([1]), k_lens])
+            .cumsum(0, dtype=torch.int32)
+            .to(q.device, non_blocking=True),
             seqused_q=None,
             seqused_k=None,
             max_seqlen_q=lq,
             max_seqlen_k=lk,
             softmax_scale=softmax_scale,
             causal=causal,
-            deterministic=deterministic)[0].unflatten(0, (b, lq))
+            deterministic=deterministic,
+        )[0].unflatten(0, (b, lq))
     else:
         assert FLASH_ATTN_2_AVAILABLE
         x = flash_attn.flash_attn_varlen_func(
             q=q,
             k=k,
             v=v,
-            cu_seqlens_q=torch.cat([q_lens.new_zeros([1]), q_lens]).cumsum(
-                0, dtype=torch.int32).to(q.device, non_blocking=True),
-            cu_seqlens_k=torch.cat([k_lens.new_zeros([1]), k_lens]).cumsum(
-                0, dtype=torch.int32).to(q.device, non_blocking=True),
+            cu_seqlens_q=torch.cat([q_lens.new_zeros([1]), q_lens])
+            .cumsum(0, dtype=torch.int32)
+            .to(q.device, non_blocking=True),
+            cu_seqlens_k=torch.cat([k_lens.new_zeros([1]), k_lens])
+            .cumsum(0, dtype=torch.int32)
+            .to(q.device, non_blocking=True),
             max_seqlen_q=lq,
             max_seqlen_k=lk,
             dropout_p=dropout_p,
             softmax_scale=softmax_scale,
             causal=causal,
             window_size=window_size,
-            deterministic=deterministic).unflatten(0, (b, lq))
+            deterministic=deterministic,
+        ).unflatten(0, (b, lq))
 
     # output
     return x.type(out_dtype)
@@ -145,7 +154,7 @@ def attention(
     v,
     q_lens=None,
     k_lens=None,
-    dropout_p=0.,
+    dropout_p=0.0,
     softmax_scale=None,
     q_scale=None,
     causal=False,
@@ -171,14 +180,11 @@ def attention(
             version=fa_version,
         )
 
-    if (
-        XFORMERS_AVAILABLE and torch.backends.mps.is_available()
-        and q.device.type == "mps"
-    ):
+    if XFORMERS_AVAILABLE and q.device.type != "cpu":
         if q_lens is not None or k_lens is not None:
             warnings.warn(
-                'Padding mask is disabled when using memory_efficient_attention. '
-                'It can have a significant impact on performance.'
+                "Padding mask is disabled when using memory_efficient_attention. "
+                "It can have a significant impact on performance."
             )
 
         q = q.transpose(1, 2).to(dtype)
@@ -195,15 +201,13 @@ def attention(
             q = q * softmax_scale
 
         attn_bias = LowerTriangularMask() if causal else None
-        out = memory_efficient_attention(
-            q, k, v, attn_bias=attn_bias, p=dropout_p
-        )
+        out = memory_efficient_attention(q, k, v, attn_bias=attn_bias, p=dropout_p)
         out = out.reshape(b, h, l, d).transpose(1, 2).contiguous()
         return out
 
     if q_lens is not None or k_lens is not None:
         warnings.warn(
-            'Padding mask is disabled when using scaled_dot_product_attention. It can have a significant impact on performance.'
+            "Padding mask is disabled when using scaled_dot_product_attention. It can have a significant impact on performance."
         )
     attn_mask = None
 
@@ -212,7 +216,8 @@ def attention(
     v = v.transpose(1, 2).to(dtype)
 
     out = torch.nn.functional.scaled_dot_product_attention(
-        q, k, v, attn_mask=attn_mask, is_causal=causal, dropout_p=dropout_p)
+        q, k, v, attn_mask=attn_mask, is_causal=causal, dropout_p=dropout_p
+    )
 
     out = out.transpose(1, 2).contiguous()
     return out
